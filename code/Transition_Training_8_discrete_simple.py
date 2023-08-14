307967f95042c89f40d641306470b2f9a81ef15e
gitnameisname <cck1835@naver.com>
2023-08-14 19:26:36 +0900

    rear rpm 추가 후 학습

--- code/Transition_Training_8_discrete_simple.py
+++ code/Transition_Training_8_discrete_simple.py
@@ -242,7 +242,7 @@ class TiltrotorTransitionTraining(gym.Env):
         # 조건 2: 피치 값 차이 | 작을 수록 좋음 | 0~90
         # reward 2는 클수록 좋게 설정하였음(positive)
         pitch_target = 4.0
-        reward_2 = (90 - np.abs(pitch_state - pitch_target)) / 90
+        reward_2 = (10 - np.abs(pitch_state - pitch_target)) / 10
         
         # 조건 3: 비행 시간 | 클수록 좋음 | 0 ~ inf, 1 timestep = 0.05 sec, 30,000 timestep = 1,500 sec = 25 min
         # 조건 3은 클수록 좋게 설정하였음(positive)
@@ -250,37 +250,37 @@ class TiltrotorTransitionTraining(gym.Env):
         
         # 조건 4: 크루즈 속도 차이 | 작을 수록 좋음 | 0~20
         # 조건 4는 클수록 좋게 설정하였음(positive)
-        Vcruise_target = 20
-        reward_4 = (20 - np.abs(self.state[3] - Vcruise_target)) / 20
+        Vcruise_target = 25
+        reward_4 = (25 - np.abs(self.state[3] - Vcruise_target)) / 25
 
         # 조건 5: 순항 고도 | 초기 고도: 0m > 15m나 0m나 대기 조건 차이 크지 않음 | 작을 수록 좋음 | -15 ~ +15
         # 조건 5는 클수록 좋게 설정하였음(positive)
-        reward_5 = (15 - np.abs(self.state[1])) / 30
+        reward_5 = (15 - np.abs(self.state[1])) / 15
 
-        # 조건 6: 프로펠러 rpm 최소화 | 작을수록 좋음 | 0 ~ 100
+        # 조건 6: 프로펠러 rpm 최소화 | 작을수록 좋음 | 0 ~ 1
         # 조건 6은 클수록 좋게 설정하였음(Positive)
-        reward_6 = (2 * (100 - self.r_rpm) + (100 - self.f_rpm)) / 300
+        reward_6 = (1 - self.r_rpm)
 
         # timestep 임계
         # dec_weight는 천천히 줄어들다 max_t_threshold 이후에는 0 값을 가짐
         # inc_weight는 천천히 증가하다 max_t_threshold 이후에는 1 값을 가짐
         max_t_threshold = 15000
-        # time step에 따라 감소하도록 하는 가중치
-        if self.state[6] < max_t_threshold:
-            dec_weight = np.exp(-15 * self.state[6] / max_t_threshold)
-        else:
-            dec_weight = 0
-        # time step에 따라 증가하도록 하는 가중치
-        if self.state[6] < max_t_threshold:
-            inc_weight = 1 - np.exp(-15 * self.state[6] / max_t_threshold)
-        else:
-            inc_weight = 1
+        # # time step에 따라 감소하도록 하는 가중치
+        # if self.state[6] < max_t_threshold:
+        #     dec_weight = np.exp(-15 * self.state[6] / max_t_threshold)
+        # else:
+        #     dec_weight = 0
+        # # time step에 따라 증가하도록 하는 가중치
+        # if self.state[6] < max_t_threshold:
+        #     inc_weight = 1 - np.exp(-15 * self.state[6] / max_t_threshold)
+        # else:
+        #     inc_weight = 1
 
         # 비행 속도가 20 미만일 경우와 20 이상일 경우 가중치를 다르게 배정
         # [틸트각, 피치, 비행 시간, 비행 속도, 순항 고도, 프로펠러 rpm]
         # [P,     P,    P,        P,        P,         P         ]
         if self.state[3] < 20:
-            weight = [1, 1, 1 / max_t_threshold, 1, 1, 1]
+            weight = [1, 1, 2 / max_t_threshold, 2, 1, 1]
         else:
             weight = [2, 2, 1 / max_t_threshold, 2, 2, 2]
 
